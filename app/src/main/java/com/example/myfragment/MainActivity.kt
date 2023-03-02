c76bfaa1a7b3798e1ee9a48d190e185a517c60b8
sukmaannda <p.sukmaananda@gmail.com>
2023-03-02 08:08:18 +0700

    first commit

---
+++ app/src/main/java/com/example/myfragment/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.myfragment
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView (R.layout.activity_main)
+
+        val viewPager = findViewById<ViewPager>(R.id.view_pager)
+        val tabs = findViewById<TabLayout>(R.id.tabs)
+
+        viewPager.adapter = ViewPagerAdapter(this, supportFragmentManager)
+        tabs.setupWithViewPager(viewPager)
+    }
+
+}
